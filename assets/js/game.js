88f7ac144ef54791d7a083bc8175aa2e065ef583
Lina C <linaclark96@gmail.com>
2017-09-09 21:16:13 -0400

    updates and changes

--- assets/js/game.js
+++ assets/js/game.js
@@ -55,18 +55,29 @@ $(document).ready(function() {
 
     $("#start-game").on("click", function() {
         $(".button").hide();
-
+        //$(".timer").show().html()
 
         function quizBuilder() {
             var output = [];
-            for (var i in quizQuestions) {
+            for (var i = 0; i < quizQuestions.length; i++) {
+                answers = [];
 
-                $("#quiz").show().html('<p>' + quizQuestions[i].question + "</p>" +
-                    '<p><input type="radio" name="pickOne" value="' + quizQuestions[i].answers.a + '">' + quizQuestions[i].answers.a + '</p>' +
-                    '<p><input type="radio" name="pickOne" value="' + quizQuestions[i].answers.b + '">' + quizQuestions[i].answers.b + '</p>' +
-                    '<p><input type="radio" name="pickOne" value="' + quizQuestions[i].answers.c + '">' + quizQuestions[i].answers.c + '</p>' +
-                    '<p><input type="radio" name="pickOne" value="' + quizQuestions[i].answers.d + '">' + quizQuestions[i].answers.d + '</p>');
+                for (var letter in question[i].answers) {
+                    answers.push(
+                        '<label>' +
+                        '<input type="radio" name="question' + i + '" value="' + letter + '">' +
+                        letter + ': ' +
+                        questions[i].answers[letter] +
+                        '</label>'
+                    );
+                    /*$("#quiz").show().html('<p>' + quizQuestions[i].question + "</p>" +
+                        '<p><input type="radio" name="pickOne" value="' + quizQuestions[i].answers.a + '">' + quizQuestions[i].answers.a + '</p>' +
+                        '<p><input type="radio" name="pickOne" value="' + quizQuestions[i].answers.b + '">' + quizQuestions[i].answers.b + '</p>' +
+                        '<p><input type="radio" name="pickOne" value="' + quizQuestions[i].answers.c + '">' + quizQuestions[i].answers.c + '</p>' +
+                        '<p><input type="radio" name="pickOne" value="' + quizQuestions[i].answers.d + '">' + quizQuestions[i].answers.d + '</p>');
 
+                */
+                }
             }
         }
         quizBuilder();
